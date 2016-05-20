835a2a6f12f6e72ae996e5895a8b8e497cd99833
nick <myfjdthink@163.com>
2016-05-20 14:59:28 +0800

    typescript 支持

---
+++ TypeScriptSamples/express_demo/routes/test.js
@@ -0,0 +1,9 @@
+"use strict";
+/**
+ * Created by nick on 16/5/20.
+ */
+const users = require('./users');
+console.log('user', typeof (users), users);
+var users2 = require('./users');
+console.log('user', typeof (users2), users2);
+//# sourceMappingURL=test.js.map
--- TypeScriptSamples/express_demo/routes/users.js
+++ TypeScriptSamples/express_demo/routes/users.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var router = express.Router();
-
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments)).next());
+    });
+};
+const express = require('express');
+let router = express.Router();
+const timeOut = function (time) {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            resolve('respond with asynv await');
+        }, time);
+    });
+};
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let result = yield timeOut(100);
+        res.send(result);
+    });
 });
-
+router.get('/aa', function (req, res, next) {
+    res.send('respond with a aaaa');
+});
+//exports.router = router;
 module.exports = router;
+//export {router}
+//# sourceMappingURL=users.js.map
