c4b31e2760367ae4aad722a1bacfc0f12098762a
nick <myfjdthink@163.com>
2016-08-14 16:18:45 +0800

    snake

--- TypeScriptSamples/express_demo/app/models/Order.ts
+++ TypeScriptSamples/express_demo/app/models/Order.ts
@@ -1,14 +1,6 @@
 'use strict';
-
-/**
- * Module dependencies.
- */
-
 import mongoose = require('mongoose');
-
-
 const Schema = mongoose.Schema;
-
 
 interface IOrder extends mongoose.Document {
   amount: string;
@@ -18,130 +10,26 @@ interface IOrder extends mongoose.Document {
   createdAt: Date;
 }
 
-/**
- * Order Schema
- */
-
 const _schema = new Schema({
-
   amount: {type: Number},
   oType: {type: Number},
   pType: {type: Number},
   status: {type: Number},
   bankcard: {type: String},
-  //product: {type: Object},
-  //products: {type: Array},
   parent_id: {type: String},
   createdAt: {type: Date, default: Date.now}
 }, {collection: 'order', id: true});
 
-/**
- * Validations
- */
-
 _schema.path('amount').required(true, 'Order amount cannot be blank');
 _schema.path('status').required(true, 'Order status cannot be blank');
 
-/**
- * Pre-remove hook
- */
-
 _schema.pre('remove', function (next) {
-  // const imager = new Imager(imagerConfig, 'S3');
-  // const files = this.image.files;
-
-  // if there are files associated with the item, remove from the cloud too
-  // imager.remove(files, function (err) {
-  //   if (err) return next(err);
-  // }, 'Order');
-
   next();
 });
 
-///**
-// * Methods
-// */
-//
-//_schema.methods = {
-//
-//  /**
-//   * Save Order and upload image
-//   *
-//   * @param {Object} images
-//   * @api private
-//   */
-//
-//  uploadAndSave: function (image) {
-//    const err = this.validateSync();
-//    if (err && err.toString()) throw new Error(err.toString());
-//    return this.save();
-//
-//    /*
-//     if (images && !images.length) return this.save();
-//     const imager = new Imager(imagerConfig, 'S3');
-//
-//     imager.upload(images, function (err, cdnUri, files) {
-//     if (err) return cb(err);
-//     if (files.length) {
-//     self.image = { cdnUri : cdnUri, files : files };
-//     }
-//     self.save(cb);
-//     }, 'Order');
-//     */
-//  }
-//};
-//
-///**
-// * Statics
-// */
-//
-//_schema.statics = {
-//  /**
-//   * List Orders
-//   *
-//   * @param {Object} options
-//   * @api private
-//   */
-//
-//  list: async function (options) {
-//    const criteria = options.criteria || {};
-//    const page = options.page || 0;
-//    const limit = options.limit || 30;
-//    await this.find(criteria)
-//      .populate('user', 'name username')
-//      .sort({createdAt: -1})
-//      .limit(limit)
-//      .skip(limit * page)
-//      .exec();
-//  }
-//};
-
-//interface IUserModel extends IOrder, mongoose.Document { }
 const _model = mongoose.model < IOrder >('Order', _schema);
 
 class Order {
-
-  ///**
-  // *
-  // * @returns {Promise<IOrder[]>}
-  // */
-  //static async list():Order[] {
-  //  let orders:Order[] = []
-  //  let err = null
-  //  try {
-  //    orders = await _model.find({}).exec()
-  //  } catch (innerErr) {
-  //    err = innerErr
-  //  }
-  //  orders = orders.map(order => {
-  //    return new Order(order)
-  //  })
-  //  return new Promise < IOrder[] >((resolve, reject) => {
-  //    err ? reject(err) : resolve(orders);
-  //  })
-  //}
-
-
   /**
    * static
    * @param id
@@ -155,7 +43,6 @@ class Order {
       })
     });
   }
-
   /**
    * static
    * @param id
@@ -170,12 +57,10 @@ class Order {
         });
     });
   }
-
   /**
    *
    */
   private _document:IOrder;
-
   /**
    * 构造器
    * @param document
@@ -183,7 +68,6 @@ class Order {
   constructor(document:IOrder) {
     this._document = document;
   }
-
   get amount():string {
     return this._document.amount;
   }
